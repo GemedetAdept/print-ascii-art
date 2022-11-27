5f843ba0f6293c12b70d89664747e437faecd3c0
Vivian Zitek <vivian.zitek@gmail.com>
2022-11-27 06:34:36 -0700

    Add file selection printout.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// Print list of options from text files in directory
 // Change color of printed text
 
+string workingDirectory = Directory.GetCurrentDirectory();
+string asciiFolder = workingDirectory + "\\asciiArtFiles";
+
+DirectoryInfo asciiFolderInfo = new DirectoryInfo(asciiFolder);
+
+FileInfo[] asciiFiles = asciiFolderInfo.GetFiles();
+
+for (int i = 0; i < asciiFiles.Length; i++) {
+
+	Console.WriteLine($"[{i}] {asciiFiles[i].Name}");
+}
